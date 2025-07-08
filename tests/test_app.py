3d9235fcca8325ff52bd3acc3ecc6f7b4243d18c
jvecodev <jvitor.oliveira1803@gmail.com>
2025-07-08 12:42:01 -0300

    feat(tests): adicionar documentação ao teste da rota raiz

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,11 +3,20 @@ from fast_api.app import app
 
 
 def test_root():
+    """
+    Esse teste tem 3 etapas: (AAA)
+    
+    1. Arrange: configurar o ambiente de teste, criando um cliente de teste
+    2. Act: chamar a rota raiz da aplicação e executa a ação
+    3. Assert: verificar se a resposta é a esperada e garante que a aplicação está funcionando corretamente
+    4. Alguns ensinadores incluem o teardown, mas nesse caso não é necessário, que no caso é a limpeza do ambiente de teste, queda do ambiente, etc.
+    """
+    # Arrange
     client = TestClient(app)
 
-    # precisamos do metodo get, a partir do client para chamar a rota de app, que esta no outro arquivo
+    # Act
     response = client.get('/')
 
-    # precisamos pergar a resposta, retornando um json
-
+    # Assert
+    assert response.status_code == 200 # Ou HTTPStatus.OK no lugar do 200
     assert response.json() == {'MSG': 'Hello World'}
